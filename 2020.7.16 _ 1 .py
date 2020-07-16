084910f487d4b962a3acfc68ea5ffd4a47dfef66
daewon kim <2000daewon@naver.com>
2020-07-16 20:57:10 +0900

    Create 2020.7.16 _ 1 .py

---
+++ 2020.7.16 _ 1 .py
@@ -0,0 +1,17 @@
+print("주석은 #처리, 프리늩 함수의 프린트는 식별자, 더하기 빼기 기호는 그냥 입력이 가능")
+print("괄호안에 암것도 없으면 줄바꿈이 가능")
+print()
+print()
+print("This is CamelCase, this is snake_case")
+print(0)#주석처리는 샵(#)표시임
+print("숫자는 따옴표 없이 입력가")
+print(1+1)
+
+
+print("이름을 붙일 때는 C하고 마찬가지로 숫자 안됨 공백 안됨 지정 키워드 안됨 ")
+
+
+
+
+
+print("----- distinguish line -----")

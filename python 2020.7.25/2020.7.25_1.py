054a7bb58eff6c63ec03b7fff10f8c945908e5f7
daewon kim <2000daewon@naver.com>
2020-07-25 21:26:33 +0900

    Create 2020.7.25_1.py
    2020.7.25

---
+++ python 2020.7.25/2020.7.25_1.py
@@ -0,0 +1,70 @@
+print("""it is if everyone dies alone
+      does that scare you I don't wanna be alone""")
+print("\n")
+
+print("""\
+      it is if everyone dies alone
+      does that scare you I don't wanna be alone\
+      """)
+
+print("안녕하세요"+"1")
+
+print("Hello"[1:4])
+print("Hello"[:3])
+
+print(len("radioactive"))
+
+print(52)
+print(31.123132)
+print(type(23))
+print(type(32.009))
+print(type("a"))
+print("사칙연산을 쓰면 결과값이 나옴",5+7)
+print("식과 값을 출력하려면 큰따옴표안에 식을 써줘야함 5+7=",5+7)
+print("정수나누기 연산자는 //(슬래쉬 2개)이다,  예시: 3/2=1.5 , 3//2=1 이렇게 출력")
+print("나머지 연산자는 %이다 5%2=",5%2)
+print("제곱연산자는 **(별 2개), 2**7=",2**7)
+print("사칙연산의 순서는 내가 아는 그대로임")
+
+print("\n")
+print("\n")
+
+pi=3.14159265
+print(pi+2)
+
+string=input("인사말을 입력하세요>")
+print(string)
+
+number= input("숫자를 입력하세요>")
+print(number)
+
+string_a=input("입력A>")
+int_a = int(string_a)
+
+string_b= input("입력B>")
+int_b=int(string_b)
+
+print("문자열 자료:", string_a+string_b)
+print("숫자 자료:",int_a+int_b)
+
+
+
+output_a=int("52")
+output_b=float("52.273")
+
+print(type(output_a),output_a)
+print(type(output_b),output_b)
+
+input_a=float(input("첫번재 숫자>"))
+input_b=float(input("두번쨰 숫자>"))
+
+print("덧셈결과:",input_a+input_b)
+print("뺄셈결과:",input_a-input_b)
+print("곱셈결과:",input_a*input_b)
+print("나눗셈 결과",input_a/input_b)
+print("\n")
+
+output_a=str(52)
+output_b=str(52.273)
+print(type(output_a),output_a)
+print(type(output_b),output_b)
